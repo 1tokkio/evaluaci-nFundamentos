aff35f4888e69f0ab70c3f92151a884c56637308
1tokkio <matiasvargas124@gmail.com>
2024-04-24 18:04:41 -0400

    ingreso de proyecto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './Components/Weather-app/WeatherApp.css'
+import WeatherApp from './Components/Weather-app/WeatherApp.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
